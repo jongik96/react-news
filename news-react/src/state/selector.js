bd289454c9d1983184392a7a4c74443d365f4ab2
jongik96 <pji3504@naver.com>
2022-05-10 16:51:33 +0900

    불필요한 파일 삭제

--- news-react/src/state/selector.js
+++ news-react/src/state/selector.js
@@ -2,29 +2,7 @@ import { selector } from "recoil";
 import { fetchAsk, fetchJobs, fetchNews } from "../api";
 import { pathNameState } from "./atoms";
 
-export const getNewsList = selector({
-  key: "getNewsList",
-  get: async () => {
-    const response = await fetchNews();
-    return response.data;
-  },
-});
-
-export const getAskList = selector({
-  key: "getAskList",
-  get: async () => {
-    const response = await fetchAsk();
-    return response.data;
-  },
-});
-
-export const getJobsList = selector({
-  key: "getJobsList",
-  get: async () => {
-    const response = await fetchJobs();
-    return response.data;
-  },
-});
+// });
 
 export const getItemList = selector({
   key: "getItemList",
